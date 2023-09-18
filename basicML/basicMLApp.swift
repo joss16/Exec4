1803fbe42839bc0f424228d3e0c4e548e091dbec
Johnny Slätt <johnny.slatt@gmail.com>
2023-09-18 14:53:34 +0200

    Initial Commit

---
+++ basicML/basicMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicMLApp.swift
+//  basicML
+//
+//  Created by BillU on 2023-09-18.
+//
+
+import SwiftUI
+
+@main
+struct basicMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
